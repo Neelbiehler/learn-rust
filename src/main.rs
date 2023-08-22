c8af7821cf4912ada9704cf1ac8b5c400c0c3859
Neel <neelbiehler@outlook.com>
2023-08-22 18:16:43 +0000

    setup project

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    println!("Rate die Zahl!");
+
+    println!("Bitte gib deine Schätzung ein.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Fehler beim Lesen der Zeile");
+
+    println!("Deine Schätzung: {guess}");
+
+}
